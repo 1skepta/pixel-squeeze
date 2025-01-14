0d1f402e3bf15da1ee51583745b569c63bc775f6
1skepta <prospaul999@gmail.com>
2025-01-14 23:07:03 +0000

    add favicon to page title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'pixel-squeeze';
+
+  constructor(private titleService: Title) {}
+  ngOnInit(): void {
+    this.titleService.setTitle(
+      'Pixel Squeeze - Your First Stop For Image Compression'
+    );
+  }
 }
