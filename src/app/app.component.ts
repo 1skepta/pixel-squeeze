400caeb29908e745ed3ed491dce30a380ea5bb04
1skepta <prospaul999@gmail.com>
2025-01-15 01:44:44 +0000

    designing body page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { HeaderComponent } from './components/header/header.component';
 import { CommonModule } from '@angular/common';
+import { BodyComponent } from './components/body/body.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, CommonModule],
+  imports: [HeaderComponent, CommonModule, BodyComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
