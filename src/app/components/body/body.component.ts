b14fa88605fea142d2754dd2f51afb2219ca561a
1skepta <prospaul999@gmail.com>
2025-01-16 23:38:37 +0000

    add footer page

--- src/app/components/body/body.component.ts
+++ src/app/components/body/body.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { StoriesComponent } from '../stories/stories.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-body',
-  imports: [StoriesComponent],
+  imports: [StoriesComponent, FooterComponent],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css',
 })
