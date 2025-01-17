f6b5c2f75ba2223df7c830cf47adb2f184882950
1skepta <prospaul999@gmail.com>
2025-01-17 00:53:12 +0000

    mobile page done

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -4,8 +4,16 @@ import { Component } from '@angular/core';
   selector: 'app-footer',
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
 export class FooterComponent {
+  sendEmail(email: string): void {
+    window.location.href = `mailto:${email}`;
+  }
 
+  currentYear: number;
+
+  constructor() {
+    this.currentYear = new Date().getFullYear();
+  }
 }
